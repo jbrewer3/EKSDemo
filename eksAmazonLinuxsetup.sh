6d26b004673dee79ad5a6ea8011ed788f7f2bca8
JoshuaBrewer <74952163+jbrewer3@users.noreply.github.com>
2022-07-14 11:46:26 -0500

    Update eksAmazonLinuxsetup.sh

--- eksAmazonLinuxsetup.sh
+++ eksAmazonLinuxsetup.sh
@@ -10,3 +10,7 @@ echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
+#update aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
